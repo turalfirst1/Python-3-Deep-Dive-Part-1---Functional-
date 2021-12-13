394a1e20283d18e3a5f33a08383205ef5a9a8a05
turalfirst1 <mrturalismayilov@gmail.com>
2021-12-13 23:35:47 +0200

    First lesson done

---
+++ Python_Type_Hierarcy.py
@@ -0,0 +1,12 @@
+# Numbers - Integral Numbers - Integer and Booleans and Non-Integral Numbers - Floats(c doubles), Complex Numbers and
+# Decimals, Fractions
+
+# Collections - Sequences, Sets and Mappings. Sequences - Mutable and Immutable sequences. Mutable - Lists.
+# Immutable - Tuples and Strings. Sets - Mutable and Immutable. Mutable - Sets, Immutable - Frozen sets. Mappings -
+# Dictionaries.
+
+# Callables - User-defined functions, Generators, Classes, Instance Methods, Class Instances (__call__()), Built-in
+# Functions (e.g. len(), open())
+# Built-in Methods (e.g. my_list.append(x))
+
+# Singletons - None, NotImplemented, Ellipsis (...)
